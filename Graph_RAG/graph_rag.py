5912e77c6da30a60c057029d57a988da503c666c
OrangeJeon <orangejwi0618@naver.com>
2026-03-31 10:03:08 +0900

    project.html sixth project complete

--- Graph_RAG/graph_rag.py
+++ Graph_RAG/graph_rag.py
@@ -5,6 +5,8 @@ from neo4j_graphrag.retrievers import Text2CypherRetriever
 from neo4j_graphrag.retrievers import VectorRetriever
 from neo4j_graphrag.embeddings.base import Embedder
 from neo4j_graphrag.generation import GraphRAG
+from neo4j import GraphDatabase
+from neo4j.time import Date
 
 # Ollama LLM 클래스 정의
 class LLMResponse:
@@ -42,7 +44,7 @@ with driver.session(database="neo4j") as session:
         lambda tx: tx.run(cypher_query,
                           movie="The Matrix").data())
     recommendations = [record['recommendation'] for record in results]
-"""
+
 def generate_embedding(text):
     response = ollama.embeddings(
         model='tinyllama',
@@ -67,7 +69,7 @@ def add_embedding_to_movie(tx):
         print(f"Updated movie '{title}' with embedding")
 with driver.session() as session:
     session.execute_write(add_embedding_to_movie)
-"""
+
 class OllamaEmbedder(Embedder):
     def __init__(self, model_name):
         self.model_name = model_name
@@ -95,4 +97,114 @@ rag = GraphRAG(retriever = retriever, llm = llm)
 query_text = "What movies are sad romances?"
 response = rag.search(query_text=query_text, retriever_config={"top_k": 5})
 print(response.answer)
+
+def get_node_datatype(value):
+    """
+    입력된 노드 value의 데이터 타입을 반환
+    """
+
+    if isinstance(value, str):
+        return "STRING"
+    elif isinstance(value, int):
+        return "INTEGER"
+    elif isinstance(value, float):
+        return "FLOAT"
+    elif isinstance(value, bool):
+        return "BOOLEAN"
+    elif isinstance(value, list):
+        return f"LIST[{get_node_datatype(value[0])}]" if value else "LIST"
+    elif isinstance(value, Date):
+        return "DATE"
+    else:
+        return "UNKNOWN"
+    
+def get_schema(uri, user, password):
+    """
+    Graph DB의 정보를 받아 노드 및 관계의 프로퍼티를 추출하고 스키마 딕셔너리를 반환
+    """
+
+    driver = GraphDatabase.driver(
+        uri, 
+        auth=basic_auth("neo4j", "interiors-representative-cranks")
+    )
+
+    with driver.session() as session:
+        #노드 프로퍼티 및 타입 추출
+        node_query="""
+        MATCH (n)
+        WITH DISTINCT labels(n) AS node_labels, keys(n) AS property_keys, n
+        UNWIND node_labels AS label
+        UNWIND property_keys AS key
+        RETURN label, key, n[key] AS sample_value
+        """
+        nodes = session.run(node_query)
+
+        #관계 프로퍼티 및 타입 추출
+        rel_query = """
+        MATCH ()-[r]->()
+        WITH DISTINCT type(r) AS rel_type, keys(r) AS property_keys, r
+        UNWIND property_keys AS key
+        RETURN rel_type, key, r[key] AS sample_value
+        """
+        relationship = session.run(rel_query)
+
+        #관계 유형 및 방향 추출
+        rel_direction_query = """
+        MATCH (a)-[r]->(b)
+        RETURN DISTINCT labels(a) AS start_label, type(r) AS rel_type, labels(b) AS end_label
+        ORDER BY start_label, rel_type, end_label
+        """
+        rel_direction = session.run(rel_direction_query)
+
+        #스키마 딕셔너리 생성
+        schema = {"nodes": {}, "relationship": {}, "relations": {}}
+
+        for record in nodes:
+            label = record["label"]
+            key = record["key"]
+            sample_value = record["sample_value"] #데이터 타입을 추론하기 위한 샘플
+            inferred_type = get_node_datatype(sample_value)
+            if label not in schema["nodes"]:
+                schema["nodes"][label] = {}
+            schema["nodes"][label][key] = inferred_type
+        
+        for record in relationship:
+            rel_type = record["rel_type"]
+            key = record["key"]
+            sample_value = record["sample_value"]
+            inferred_type = get_node_datatype(sample_value)
+            if rel_type not in schema["relationship"]:
+                schema["relationship"][rel_type] = {}
+            schema["relationship"][rel_type][key] = inferred_type
+
+        for record in rel_direction:
+            start_label = record["start_label"][0]
+            rel_type = record["rel_type"]
+            end_label = record["end_label"][0]
+            schema["relations"].append(f"(:{start_label})=[:{rel_type}]->(:{end_label})")
+        return schema
+    
+    def format_schema(schema):
+        """
+        스키마 딕셔너리를 LLM에 제공하기 위해 원하는 형태로 formatting
+        """
+        result = []
+
+        #노드 프로퍼티 출력
+        result.append("Node Properties")
+        for label, properties in schema["nodes"].items():
+            props = ", ".join(f"{k}: {v}" for k, v in properties.items())
+            result.append(f"{label} {{{props}}}")
+
+        #관계 프로퍼티 추가
+        result.append("Relationship Properties")
+        for rel_type, properties in schema["relationship"].items():
+            props = ",".join(f"{k}, {v}" for k, v in properties.items())
+            result.append(f"{rel_type} {{{props}}}")
+
+        #관계 프로퍼티 출력
+        result.append("The Relationship: ")
+        for relation in schema["relations"]:
+            result.append(relation)
+        return "\n".join(result)
 
